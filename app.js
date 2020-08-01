efdac7354007506e23162a39ce28cb3a6afebee2
MohinishTeja <45705872+MohinishTeja@users.noreply.github.com>
2020-08-01 21:01:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+VANTA.DOTS({
+    el: "#intro",
+    mouseControls: true,
+    touchControls: true,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x20fffc,
+    color2: 0x20f4ff
+  })
